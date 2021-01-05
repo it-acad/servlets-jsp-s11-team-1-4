c2f46ada1a02a2a59e57b56989ec77efc9264cb1
Ostap Tsohla <tsohlaostap@gmail.com>
2021-01-05 20:56:24 +0200

    Update TestServlet.java

--- src/main/java/com/softserve/itacademy/controller/TestServlet.java
+++ src/main/java/com/softserve/itacademy/controller/TestServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(value = "/")
+@WebServlet("/test")
 public class TestServlet extends HttpServlet {
 
     private TaskRepository taskRepository;
